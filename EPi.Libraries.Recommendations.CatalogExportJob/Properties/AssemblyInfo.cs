8dc471277c1b816fbac58991c97697563df1ad5f
Jeroen <jeroen.developer@outlook.com>
2016-09-11 15:26:59 +0200

    Added missing files

---
+++ EPi.Libraries.Recommendations.CatalogExportJob/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EPi.Libraries.Recommendations.CatalogExportJob")]
+[assembly: AssemblyDescription("Export catalogs to Microsoft Cognitive Services Recommendations API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jeroen Stemerdink")]
+[assembly: AssemblyProduct("EPi.Libraries.Recommendations.CatalogExportJob")]
+[assembly: AssemblyCopyright("Copyright © Jeroen Stemerdink  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("36536b44-daeb-4dcd-ac0d-ea8902b5daf8")]
